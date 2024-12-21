c1cf9d7038dde03281f5394f0a1d1bae2a50b155
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-12-21 03:47:18 -0300

    ✨ feat : side scrolling of skills

--- src/script.js
+++ src/script.js
@@ -2,3 +2,6 @@ window.addEventListener("scroll", function () {
   let header = this.document.querySelector("#header");
   header.classList.toggle("rolagem", this.window.scrollY > 0);
 });
+
+let copy = document.querySelector(".line-skills").cloneNode(true);
+document.querySelector('.container-skills').appendChild(copy)
